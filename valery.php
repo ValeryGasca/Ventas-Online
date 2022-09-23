e1d9e755dd7856a8b5463f84286ffe77d00939f0
ValeryGasca <114257844+ValeryGasca@users.noreply.github.com>
2022-09-23 14:26:24 -0500

    Add files via upload

---
+++ valery.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Funcion printf</title>
+</head>
+<body color='#303030' text='#a0a0a0'>
+   <body leftmargin='40'>
+   <body rightmargin='40'>   
+   <body topmargin='40'>   
+       <font face='roboto' size='4'>
+<h1>Informacion Personal Mile</h1>
+<hr size='20' color='FFD700' with='40%' align='left'>
+<?php
+$var='programacion en la nube';
+echo'<br>';
+echo'<br>';
+echo(strtolower($var));
+echo'<br>';
+echo'<br>';
+echo(strtoupper($var));
+?>
+<hr size='20' color='FFD700' with='40%' align='left'>
+//hgcgvgvg
